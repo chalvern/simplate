b39ade397da518a2a5ddcb4f24d897cb334fe5ed
Jingwei <zhjw43@163.com>
2019-08-22 14:36:29 +0800

    rename HTMLRender as GinRender

--- gin.go
+++ gin.go
@@ -7,13 +7,13 @@ import (
 
 // for gin https://github.com/gin-gonic/gin
 
-// HTMLRenderer html render of gin
-type HTMLRenderer struct {
+// GinRenderer html render of gin
+type GinRenderer struct {
 }
 
 // Instance method of HTMLRender
 // https://github.com/gin-gonic/gin/blob/9a820cf0054bcd769f785457b7dbd149a7b29fdd/render/html.go#L21
-func (hr *HTMLRenderer) Instance(name string, data interface{}) render.Render {
+func (hr *GinRenderer) Instance(name string, data interface{}) render.Render {
 	if simplateViewPathTemplates[name] == nil {
 		sugar.Warnf("no template of name: %s", name)
 	}
