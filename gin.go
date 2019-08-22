725da9402495db2d9ba918216d0af99dc778b7b7
Jingwei <zhjw43@163.com>
2019-08-22 14:33:49 +0800

    add warning of Instance

--- gin.go
+++ gin.go
@@ -1,6 +1,7 @@
 package simplate
 
 import (
+	"github.com/chalvern/sugar"
 	"github.com/gin-gonic/gin/render"
 )
 
@@ -13,6 +14,9 @@ type HTMLRenderer struct {
 // Instance method of HTMLRender
 // https://github.com/gin-gonic/gin/blob/9a820cf0054bcd769f785457b7dbd149a7b29fdd/render/html.go#L21
 func (hr *HTMLRenderer) Instance(name string, data interface{}) render.Render {
+	if simplateViewPathTemplates[name] == nil {
+		sugar.Warnf("no template of name: %s", name)
+	}
 	return render.HTML{
 		Template: simplateViewPathTemplates[name],
 		Data:     data,
